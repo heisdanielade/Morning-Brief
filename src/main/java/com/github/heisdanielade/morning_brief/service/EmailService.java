97d3bebe70c8c56d46d616c2151b6e57cc1fc443
heisdanielade <heisdanielade@gmail.com>
2025-03-27 21:00:51 +0100

    minor changes

--- src/main/java/com/github/heisdanielade/morning_brief/service/EmailService.java
+++ src/main/java/com/github/heisdanielade/morning_brief/service/EmailService.java
@@ -2,12 +2,14 @@ package com.github.heisdanielade.morning_brief.service;
 
 import jakarta.mail.MessagingException;
 import jakarta.mail.internet.MimeMessage;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
 
 @Service
 public class EmailService {
+    @Autowired
     private JavaMailSender emailSender;
 
     public void sendEmail(String to, String subject, String text) throws MessagingException {
