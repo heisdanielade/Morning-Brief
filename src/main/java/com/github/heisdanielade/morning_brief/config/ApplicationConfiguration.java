a81f3dbdeda24df53162748c9fb4dcba71395ba1
heisdanielade <heisdanielade@gmail.com>
2025-03-27 21:16:49 +0100

    update: added rest template bean in app config

--- src/main/java/com/github/heisdanielade/morning_brief/config/ApplicationConfiguration.java
+++ src/main/java/com/github/heisdanielade/morning_brief/config/ApplicationConfiguration.java
@@ -1,4 +1,14 @@
 package com.github.heisdanielade.morning_brief.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+@Configuration
 public class ApplicationConfiguration {
+
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
 }
