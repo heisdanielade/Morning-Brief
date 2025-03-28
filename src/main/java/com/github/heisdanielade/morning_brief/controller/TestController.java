80cfa557d9c6f3108712f7325590d787f975e245
heisdanielade <heisdanielade@gmail.com>
2025-03-28 10:29:12 +0100

    update: minor changes to test controller endpoint

--- src/main/java/com/github/heisdanielade/morning_brief/controller/TestController.java
+++ src/main/java/com/github/heisdanielade/morning_brief/controller/TestController.java
@@ -1,6 +1,6 @@
 package com.github.heisdanielade.morning_brief.controller;
 
-import com.github.heisdanielade.morning_brief.service.DailySummaryService;
+
 import com.github.heisdanielade.morning_brief.service.EmailService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,19 +11,15 @@ import org.springframework.web.bind.annotation.RestController;
 public class TestController {
 
     private final EmailService emailService;
-    private final DailySummaryService dailySummaryService;
 
-    public TestController(EmailService emailService, DailySummaryService dailySummaryService) {
+    public TestController(EmailService emailService) {
         this.emailService = emailService;
-        this.dailySummaryService = dailySummaryService;
     }
 
     @GetMapping("/test-email")
     public String testEmail() {
-        String emailContent = dailySummaryService.generateDailyBrief();
         String recipient = "danieladeofficial@gmail.com";
-        String emailSubject = "🌞 Your Morning Brief";
-        emailService.sendEmail(recipient, emailSubject, emailContent);
+        emailService.sendDailyBriefEmail(recipient);
         return "Test email sent successfully!";
     }
 }
