3fa87ef8a932e993b6e0ce2125e1e173cd617a39
heisdanielade <heisdanielade@gmail.com>
2025-03-28 08:57:26 +0100

    update: added service logic to schedule email sending at 7am everyday using a cronjob

--- src/main/java/com/github/heisdanielade/morning_brief/service/ScheduleTaskService.java
+++ src/main/java/com/github/heisdanielade/morning_brief/service/ScheduleTaskService.java
@@ -1,4 +1,24 @@
 package com.github.heisdanielade.morning_brief.service;
 
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Component
 public class ScheduleTaskService {
+
+    private final EmailService emailService;
+    private final DailySummaryService dailySummaryService;
+
+    public ScheduleTaskService(EmailService emailService, DailySummaryService dailySummaryService) {
+        this.emailService = emailService;
+        this.dailySummaryService = dailySummaryService;
+    }
+
+    @Scheduled(cron = "0 0 7 * * ?") // Every day at 7 AM
+    public void sendDailyEmail() {
+        String emailContent = dailySummaryService.generateDailyBrief();
+        String recipient = "danieladeofficial@gmail.com";
+        String emailSubject = "🌞 Your Morning Brief";
+        emailService.sendEmail(recipient, emailSubject, emailContent);
+    }
 }
